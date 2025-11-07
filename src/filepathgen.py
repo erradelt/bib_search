62c7957b8e6606b588d8d660a7dc85b4b31fbbdb
radelt <137507960+erradelt@users.noreply.github.com>
2025-11-07 11:57:23 +0100

    Delete src directory

--- src/filepathgen.py
+++
@@ -1,3 +0,0 @@
-import os
-
-current_directory = os.path.dirname(os.path.abspath(__file__))
--- src/path_manager_logic.py
+++
@@ -1,8 +0,0 @@
-from PyQt5 import QtWidgets
-from path_manager_ui import Ui_Dialog
-
-class PathManager(QtWidgets.QDialog):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_Dialog()
-        self.ui.setupUi(self)
